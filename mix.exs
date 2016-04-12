d45027cb0e4202b6861bc5e8ad580c061b67e6f2
Paul Hieromnimon <paul.hieromnimon@gmail.com>
2016-04-12 06:19:54 -0700

    bump to version 0.1.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule GenFSM.Mixfile do
 
   def project do
     [app: :gen_fsm,
-     version: "0.0.4",
+     version: "0.1.0",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
